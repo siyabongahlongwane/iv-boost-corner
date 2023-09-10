6726a41fb92eedc0ff935cf3ca1291ef036eb065
siyabonga-hlongwane <siyabonga.hlongwane@4sure-net>
2023-09-10 11:47:03 +0200

    Fixed Topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,22 +1,24 @@
 const Topbar = () => {
   return (
-    <div className="topbar white-col d-flex flex-row flex-col-xs justify-content-between align-items-center">
+    <div className="topbar black-bg white-col d-flex flex-row flex-col-xs justify-content-between align-items-center">
       <div className="d-flex flex-col justify-content-center">
-        <div className="d-flex flex-row flex-col-xs align-items-center-xs gap-med gap-xs">
+        <div className="d-flex d-flex-row align-items-center-xs gap-med gap-xs flex-col-xs">
           <div className="d-flex flex-row gap-std">
             <i className="fa fa-phone primary-col"></i>
             <small>062 309 6853</small>
           </div>
-          <div className="d-flex flex-row gap-std justify-content-center-xs block align-center-xs">
-            
-            <small><i className="fa fa-thumb-tack primary-col mr-1em"></i> 07 Sunrock Village, 131 Agulhas Rd, Klipportjie, 1459</small>
+          <div className="d-flex flex-row gap-std">
+            <i className="fa fa-thumb-tack primary-col"></i>
+            <small>07 Sunrock Village, 131 Agulhas Rd, Klipportjie, 1459</small>
           </div>
         </div>
       </div>
 
       <div className="d-flex flex-row gap-std align-items-center">
         <small>Socials: </small>
-        <a href="https://wa.me/27623096853?body='Hello, I am looking to get assistance with'"><i className="fa fa-whatsapp icon primary-col"></i></a>
+        <a href="https://wa.me/27623096853?body='Hello, I am looking to get assistance with'">
+          <i className="fa fa-whatsapp icon primary-col"></i>
+        </a>
         {/* <a href="https://wa.me/27623096853?body='Hello, I am looking to get assistance with'"><i className="fa fa-facebook icon blue-col"></i></a> */}
       </div>
     </div>
